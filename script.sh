5567d0c77b86c56add1efe85de5bd0bcf39f55be
Jannis <83124510+DerPandaa1@users.noreply.github.com>
2024-04-24 12:29:59 +0200

    switch case

--- script.sh
+++ script.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 boolvar=`cat automatedMode`
 if [[ $boolvar -eq 1 ]]; then
-  echo 1
-  for filename in `find ~/Downloads/ -type f -mtime -1`
+  for file in `find ~/Downloads/ -type f -mtime -1`
   do 
-    echo "$filename"
+    case in #JavaProgram
+      Englisch)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/Englisch/src/$file
+        ;;
+      GIT)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/GIT/src/$file
+        ;;
+      Politik)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/Politik/src/$file
+        ;;
+      Deutsch)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/Deutsch/src/$file
+        ;;
+      Wirtschaft)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/Wirtschaft/src/$file
+        ;;
+      EVP)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/EVP/src/$file
+        ;;
+      STD)
+        mv $file ~/obsidian_vault/1.Jahr/RBBK 1. Jahr/STD/src/$file
+        ;;
+      *)
+        #others
+        ;;
+
   done
 else
-  echo 0
 fi

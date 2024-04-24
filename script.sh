9e5657eca6cc578ea959984a538c56623bbe262a
Jannis <83124510+DerPandaa1@users.noreply.github.com>
2024-04-24 12:14:46 +0200

    Added AutomatedMode
    reads from FIle "automatedMode" if 1, everything schould be automated

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-for filename in ~/Downloads/*;
-do 
-  echo "$filename"
-done
+boolvar=`cat automatedMode`
+if [[ $boolvar -eq 1 ]]; then
+  echo 1
+  for filename in ~/Downloads/*;
+  do 
+    echo "$filename"
+  done
+else
+  echo 0
+fi
